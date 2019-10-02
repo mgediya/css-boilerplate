5c48c06156668c91973b645990de085182120add
kanaksinh-zwt <kanaksinh@zealousweb.com>
2019-10-02 14:35:02 +0530

    Update 1.1.1 to 1.1.2
    some issue fixed and some files updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,69 +2,64 @@ var $ = jQuery.noConflict();
 
 /* Script on ready
 ------------------------------------------------------------------------------*/
-$(document).ready(function(){
-    //do jQuery stuff when DOM is ready
+$( document ).ready( function() {
+	//do jQuery stuff when DOM is ready
 
-    /* Responsive Jquery Navigation */
-    $('.hamburger').click(function(event){
-        $('#mobilenav').toggleClass('is-open');
-    });
-    $('#mobilenav .nav-backdrop').click(function(){
-        $('#mobilenav').removeClass('is-open');
-    });
+	/* Responsive Jquery Navigation */
+	$( '.hamburger' ).click( function( event ) {
+		$( '.mobilenav' ).toggleClass( 'is-open' );
+	} );
 
-    $('body').on('keyup',function(evt) {
-        if (evt.keyCode == 27) {
-            $('.hamburger.is-active').removeClass('is-active');
-            $('#mobilenav.is-show').removeClass('is-show');
-        }
-    });
+	$( '.mobilenav .nav-backdrop' ).click( function() {
+		$( '.mobilenav' ).removeClass( 'is-open' );
+	} );
 
-    var clickable = $('#menu').attr('link-clickable');
-    $('#mobilenav li:has(ul)').addClass('has-sub');
-    $('#mobilenav .has-sub>a').after('<em class="caret">');
-    if(clickable == 'true'){
-        $('#mobilenav .has-sub>a').addClass('trigger-caret').attr('href','javascript:;');
-    }else{
-        $('#mobilenav .has-sub>.caret').addClass('trigger-caret');
-    }
+	var clickable = $( '.menu-state' ).attr( 'data-clickable' );
+	$( '.mobilenav li:has(ul)' ).addClass( 'has-sub' );
+	$( '.mobilenav .has-sub>a' ).after( '<em class="caret">' );
+	if ( clickable == 'true' ) {
+		$( '.mobilenav .has-sub>.caret' ).addClass( 'trigger-caret' );
+	} else {
+		$( '.mobilenav .has-sub>a' ).addClass( 'trigger-caret' ).attr( 'href','javascript:;' );
+	}
 
-    /* menu open and close on single click */
-    $(document).on('click','#mobilenav .has-sub>.trigger-caret',function(event){
-        var element = $(this).parent('li');
-        if (element.hasClass('is-open')) {
-            element.removeClass('is-open');
-            element.find('li').removeClass('is-open');
-            element.find('ul').slideUp(200);
-        }
-        else {
-            element.addClass('is-open');
-            element.children('ul').slideDown(200);
-            element.siblings('li').children('ul').slideUp(200);
-            element.siblings('li').removeClass('is-open');
-            element.siblings('li').find('li').removeClass('is-open');
-            element.siblings('li').find('ul').slideUp(200);
-        }
-    });    
-});
+	/* menu open and close on single click */
+	$( '.mobilenav .has-sub>.trigger-caret' ).click( function() {
+		var element = $( this ).parent( 'li' );
+		if ( element.hasClass( 'is-open' ) ) {
+			element.removeClass( 'is-open' );
+			element.find( 'li' ).removeClass( 'is-open' );
+			element.find( 'ul' ).slideUp( 200 );
+		}
+		else {
+			element.addClass( 'is-open' );
+			element.children( 'ul' ).slideDown( 200 ) ;
+			element.siblings( 'li' ).children( 'ul' ).slideUp( 200 );
+			element.siblings( 'li' ).removeClass( 'is-open' );
+			element.siblings( 'li' ).find( 'li' ).removeClass( 'is-open' );
+			element.siblings( 'li' ).find( 'ul' ).slideUp( 200 );
+		}
+	} );
+} );
 
 /* Script on load
 ------------------------------------------------------------------------------*/
-$(window).load(function() {
-    // page is fully loaded, including all frames, objects and images
-});
+$( window ).on( 'load',function() {
+	// page is fully loaded, including all frames, objects and images
+
+} );
 
 /* Script on scroll
 ------------------------------------------------------------------------------*/
-$(window).scroll(function() {
+$( window ).on( 'scroll',function() {
 
-});
+} );
 
 /* Script on resize
 ------------------------------------------------------------------------------*/
-$(window).resize(function() {
+$( window ).on( 'resize',function() {
 
-});
+} );
 
 /* Script all functions
 ------------------------------------------------------------------------------*/
